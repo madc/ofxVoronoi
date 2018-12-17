96220630612a44d4eed84176fe0abbc43e8e438e
valillon <valillon@gmail.com>
2018-12-17 16:34:49 +0100

    Updated example

--- example_basic/src/ofApp.h
+++ example_basic/src/ofApp.h
@@ -4,25 +4,30 @@
 #include "ofxVoronoi.h"
 
 class ofApp : public ofBaseApp{
-    private:
-        vector <ofPoint> generateRandomPoints(int count, int seed, ofRectangle bounds);
-        ofxVoronoi voronoi;
-        vector<ofPoint> points;
-    
-        bool isBorder(ofPoint _pt);
-    
-	public:
-		void setup();
-		void update();
-		void draw();
 
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
+private:
+
+    // For legacy openGL format change OF_USE_LEGACY_VECTOR_MATH to 1 in ofConstants.h
+    // and also you could use explicit ofPoint instead of glm:vec3 in ofApp.hpp and ofApp.cpp
+    vector<ofDefaultVec3> points;
+    ofxVoronoi voronoi;
+
+    bool isBorder(ofDefaultVec3 _pt);
+
+public:
+
+    void setup();
+    void update();
+    void draw();
+
+    void keyPressed(int key);
+    void keyReleased(int key);
+    void mouseMoved(int x, int y );
+    void mouseDragged(int x, int y, int button);
+    void mousePressed(int x, int y, int button);
+    void mouseReleased(int x, int y, int button);
+    void windowResized(int w, int h);
+    void dragEvent(ofDragInfo dragInfo);
+    void gotMessage(ofMessage msg);
+    
 };
